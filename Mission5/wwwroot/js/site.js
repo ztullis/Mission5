69de7bf5cbc13108dc04026d3703270360e6cf02
Zach Tullis <zachjtullis@gmail.com>
2024-02-20 17:41:15 -0700

    final commit again?

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function ()
+{
+    $('#calculate').click(function ()
+    {
+        var hours = parseFloat($('#hours').val());
+        var rate = 35; // hourly rate
+
+        if (isNaN(hours) || hours < 0)
+        {
+            alert('Please enter a positive number of hours.');
+        }
+        else
+        {
+            var total = hours * rate;
+            $('#total').val('$' + total.toFixed(2));
+        }
+    });
+});
